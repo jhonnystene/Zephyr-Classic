94d0a0d6ee5e21b4d2bb17d480b6f53afd7069f0
Johnny Stene <jhonnystene@gmail.com>
2020-09-05 17:34:20 -0600

    A bit of code documentation

--- zephyr/__init__.py
+++ zephyr/__init__.py
@@ -1,52 +1,66 @@
 import sys
 
 registerNames = ["eax", "ebx", "ecx", "edx", "ax", "ah", "al", "bx", "bh", "bl", "cx", "ch", "cl", "dx", "dh", "dl", "si", "di"]
-builtins = ["inturrupt"]
+builtins = ["inturrupt", "push_all", "pop_all"]
 def stripTabs(code):
 	code = code.replace("\t", "")
 	return code
 
 class Function:
 	def __init__(self, code, name):
-		self.code = stripTabs(code)
-		self.name = name
+		self.code = stripTabs(code) # Make sure we don't have any tabs messing up our code
+		self.name = name # Set our function name
+		
+		# Parse function code into ASM
+		# TODO: If/else statements, variables
 		self.asm = ""
 		for line in self.code.split("\n"):
-			line = line[:-1]
-			if(line.startswith("//") or line == ""):
+			line = line[:-1] # Get rid of semicolon
+			if(line.startswith("//") or line == ""): # Ignore empty lines and comments
 				pass
 			elif("=" in line): # Variable or register set
-				varname = line.split(" ")[0].lower()
-				varvalue = line.split(" ")[2]
-				if(varname in registerNames):
+				varname = line.split(" ")[0].lower() # Get var/reg name
+				varvalue = line.split(" ")[2] # Get desired value
+				
+				if(varname in registerNames): # Register?
 					self.asm += "mov " + varname + ", " + varvalue + "\n"
-				else:
-					print("Error in function \"" + self.name + "\": variables not supported yet!")
+				else: # Variable?
+					print("Error in function \"" + self.name + "\": variables not supported yet!") # Not supported yet.
 					sys.exit(3)
 			elif("(" in line and ")" in line): # Function call
-				command = line.split("(")
-				command[1] = command[1][:-1]
-				if(command[0] in builtins):
-					if(command[0] == "inturrupt"):
+				command = line.split("(") # Split command and argument
+				command[1] = command[1][:-1] # Remove ) from argument
+				if(command[0] in builtins): # Are we trying to use a built-in function?
+					if(command[0] == "inturrupt"): # INT?
 						self.asm += "int " + command[1] + "\n"
-				else:
+					elif(command[0] == "push_all"): # PUSHA?
+						self.asm += "pusha"
+					elif(command[0] == "pop_all"): # POPA?
+						self.asm += "popa"
+				else: # Only call commands for now. Maybe JMP support in future?
 					self.asm += "call " + command[0] + "\n"
-			elif(line == "return"):
+			elif(line == "return"): # Returning from function?
 				self.asm += "ret\n"
 			else:
 				print("Error in function code! (" + line + " invalid)") # TODO: Print line number
 				sys.exit(3)
+		
+		# Check if function has return statement
 		if("\nret\n" not in self.asm):
 			print("Warning! Function \"" + self.name + "\" has no return!")
 
 class SourceFile:
+	# TODO: #include - Include a file
+	# TODO: #mainfunc - Define main function
+	
 	def __init__(self, code):
 		print("Parsing source code...")
-		self.code = stripTabs(code)
-		self.functions = {}
-		code = self.code.split("\n")
-		line = 0
+		self.code = stripTabs(code) # Strip tabs
+		self.functions = {} # Create function list
+		code = self.code.split("\n") # Split up into newlines so we can loop through
 		
+		# Not a for loop because we're about to jump all over the place
+		line = 0
 		while(line != len(code)):
 			if(code[line].startswith("//") or code[line] == ""): # Comment or empty line?
 				pass # Ignore it
@@ -55,6 +69,7 @@ class SourceFile:
 				
 			elif("{" in code[line]): # Starting a function?
 				if("func" in code[line]):
+					# Sort out the name from the code and store it in a new Function object
 					func = ""
 					funcName = code[line].split(" ")[1]
 					cLine = ""
@@ -65,7 +80,6 @@ class SourceFile:
 						
 					func = Function(func, funcName)
 					self.functions[funcName] = func
-					
 				else:
 					print("Error on line " + str(line + 1) + ": Misplaced '{'")
 					sys.exit(3)
