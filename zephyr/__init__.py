e39df4ef831a4e4e70c6e2ed8353d998106b4165
Johnny Stene <jhonnystene@gmail.com>
2020-09-06 11:59:07 -0600

    Don't allow special characters as variable/function names

--- zephyr/__init__.py
+++ zephyr/__init__.py
@@ -3,6 +3,7 @@ import sys
 registers = ["eax", "ebx", "ecx", "edx", "ax", "ah", "al", "bx", "bh", "bl", "cx", "ch", "cl", "dx", "dh", "dl", "si", "di"]
 builtins = ["inturrupt", "push_all", "pop_all", "hang"]
 reserved = ["byte", "word", "dword", "string"]
+disallowed = "`~!@#$%^&*()-_=+[]{}\\|/?.>,<\'\";: \n\t"
 
 def stripTabs(code):
 	code = code.replace("\t", "")
@@ -13,6 +14,10 @@ def error(string, code):
 	sys.exit(code)
 
 def isReserved(string):
+	for character in disallowed:
+		if(character in string):
+			return True
+			
 	if(string in registers or string in builtins or string in reserved):
 		return True
 	return False
