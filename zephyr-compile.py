3c2fdb21eea47d8a491917b52a1e06e72c93179c
Johnny Stene <jhonnystene@gmail.com>
2020-10-24 22:12:52 -0600

    Add output file support

--- zephyr-compile.py
+++ zephyr-compile.py
@@ -15,8 +15,14 @@ from zephyr import SourceFile
 try:
 	fileName = sys.argv[1]
 except:
-	print("Usage: zephyr-compile <zephyr source file>")
+	print("Usage: zephyr-compile <zephyr source file> <optional: output filename>")
 	sys.exit(1)
+
+try:
+	outFileName = sys.argv[2]
+except:
+	print("WARN: No output file.")
+	outFileName = False
 
 try:
 	fileContents = open(fileName).read()
@@ -25,7 +31,13 @@ except:
 	sys.exit(2)
 
 sourceFile = SourceFile.createFrom(fileContents)
-print("Generated source code: ")
-print("")
-print(sourceFile.genASM())
+if(outFileName):
+	pass
+	with open(outFileName, "w") as outfile:
+		outfile.write(sourceFile.genASM())
+	print("Compile success!")
+else:
+	print("Generated source code: ")
+	print("")
+	print(sourceFile.genASM())
 sys.exit(0)
